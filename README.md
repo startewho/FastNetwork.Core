2a023519eb99ca6c33b5d22fa982d81d36d0ff5f
startewho <hoolooday@gmail.com>
2018-11-18 19:24:27 +0800

    移植开始
    移植开始

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FastNetwork.Core
+
+移植.Netframwork下的IOCP类库
+
+https://github.com/zhujunxxxxx/FastNetwork
+
---
+++ Test/TcpClientTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TcpClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
